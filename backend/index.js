117890eb817853ab8e5d1d30482cc3b15a0307c1
Zaheer Mulani <154722247+RBZAHEER@users.noreply.github.com>
2024-10-02 13:49:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,11 +21,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-<<<<<<< HEAD
     origin: [process.env.FRONTEND_URL || "https://blog-verse-sandy.vercel.app"],
-=======
-    origin: process.env.FRONTEND_URL,
->>>>>>> f8cf5140aace45b300bcc43c901658d8adfa960b
     credentials: true,
   })
 );
